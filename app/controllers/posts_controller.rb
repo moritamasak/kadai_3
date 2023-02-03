617a1fc985414ad7342b255f54ad56530273f580
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-02-03 12:02:15 +0900

    編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
-
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def new
@@ -19,28 +17,30 @@ class PostsController < ApplicationController
   end
 
   def show
-   
+    @post = Post.find(params[:id])
   end
 
   def edit
-    
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "つぶやきを編集しました！"
     else
       render :edit
     end
   end
+  def destory
+    @post = Post.find(params[:id])
+    @post.destory
+    redirect_to posts_path, notice: "ブログを削除しました！"
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
-  end
-
-  def set_post
-    @post = Post.find(params[:id])
   end
 end
