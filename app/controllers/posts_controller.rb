8ad086c958baf4d32dba914fb198e9ec6d21c714
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-02-03 22:02:10 +0900

    再投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,14 @@ class PostsController < ApplicationController
 
   def create 
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: "つぶやきを作成しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "つぶやきを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -37,8 +41,12 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to posts_path, notice: "ブログを削除しました！"
   end
-
-  private
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+  
+    private
 
   def post_params
     params.require(:post).permit(:content)
